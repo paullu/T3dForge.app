6a398877f7dde417d92551b717ae2cf84d034e12
云燃 <yunran.lp@alibaba-inc.com>
2018-07-11 00:46:07 +0800

    update publish files

--- Contents/Resources/app/dist/electron/editorProjectCreator.js
+++ Contents/Resources/app/dist/electron/editorProjectCreator.js
@@ -62,7 +62,7 @@ module.exports =
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "7d1980d53ede6570054e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "47c28a067973b20a5ba9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
